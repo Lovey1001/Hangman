c5777110409d639a6f8e690abe03a72e6d881ae5
Lovey <lovepreetdhunna98@gmail.com>
2024-02-18 20:02:19 -0800

    starting to make tests for the hangman class

--- src/test/model/HangmanTest.java
+++ src/test/model/HangmanTest.java
@@ -6,5 +6,71 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HangmanTest {
 
+    private Hangman game1;
 
+    @BeforeEach
+    public void runBefore() {
+        game1 = new Hangman("Computer");
+    }
+
+    @Test
+    public void testConstructor() {
+        assertEquals(6, game1.numberOfLivesLeft());
+        assertTrue(game1.getGuessedLetters().isEmpty());
+    }
+
+    @Test
+    public void guessNextLetterCorrectTest() {
+
+    }
+
+    @Test
+    public void guessNextLetterIncorrectTest() {
+
+    }
+
+    @Test
+    public void guessNext2LettersCorrectTest() {
+
+    }
+
+    @Test
+    public void guessNext2LettersIncorrectTest() {
+
+    }
+
+    @Test
+    public void guessNextLetterRepeatedTest() {
+
+    }
+
+    @Test
+    public void wordGuessedCompletelyTest() {
+
+    }
+
+    @Test
+    public void wordGuessedPartiallyTest() {
+
+    }
+
+    @Test
+    public void getWordWithGuessesContainsLetterTest() {
+
+    }
+
+    @Test
+    public void getWordWithGuessesNotContainsLetterTest() {
+
+    }
+
+    @Test
+    public void isGameOverTrueTest() {
+
+    }
+
+    @Test
+    public void isGameOverFalseTest() {
+
+    }
 }
