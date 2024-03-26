288c66aaa8f3e2a42a3b28faf58a339579bacb3a
Lovey <lovepreetdhunna98@gmail.com>
2024-03-25 21:40:43 -0700

    Put the specifications for all the methods required in the HangmanGameGUI class

--- src/main/ui/HangmanGameGUI.java
+++ src/main/ui/HangmanGameGUI.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 
 //  Hangman Game application in Console.
 // Used the TellerApp application for some code functions.
-public class HangmanGameGUI {
+public class HangmanGameGUI extends JFrame {
 
     private static final String JSON_STORE = "./data/hangman.json";
     private final Scanner scanner = new Scanner(System.in);
@@ -47,171 +47,102 @@ public class HangmanGameGUI {
 
     // EFFECTS: runs the hangman game
     public HangmanGameGUI(WordList wordList) throws FileNotFoundException {
-        this.wordList = wordList;
-        this.hangmanGame = new Hangman("Hangman");
-        jsonWriter = new JsonWriter(JSON_STORE);
-        jsonReader = new JsonReader(JSON_STORE);
-        startGame();
+        // TODO
     }
 
-    // REQUIRES: no numbers or special characters allowed
-    // EFFECTS: displays the words inputted by user and added to list
-    public void addWordsMenu() {
-
-        System.out.println("Please add words to the list. Type 'done' when finished:");
-
-        while (true) {
-            String word = scanner.nextLine().trim();
-            if ("done".equalsIgnoreCase(word)) {
-                break;
-            }
-            if (!word.isEmpty() && word.matches("^[a-zA-Z]+$")) {
-                if (wordList.containsWordInList(word)) {
-                    System.out.println("This word already exists. Please enter a different word or type 'done':");
-                } else {
-                    wordList.addWordToList(word);
-                    System.out.println("Word added. Add another word or type 'done':");
-                }
-            } else {
-                System.out.println("Please add an appropriate word or type 'done':");
-            }
-        }
-        listOfWordsAddedMenu();
+    // EFFECTS: used to prepare the game field
+    private void initializeGame() {
+        // TODO
     }
 
-    // EFFECTS: displays the list of words added; works tandem with displayWordsMenu()
-    public void listOfWordsAddedMenu() {
-        System.out.println("Would you like to view the list of added words? (yes/no)");
-        String answer = scanner.nextLine().trim().toLowerCase();
-        if ("yes".equalsIgnoreCase(answer)) {
-            displayWordsMenu();
-        }
+    // EFFECTS: used to prepare the components used in the gane
+    public void initializeComponents() {
+        // TODO
     }
 
-    // EFFECTS: processes user input and runs the application
-    public void startGame() {
-
-        boolean keepGoing = true;
-
-        while (keepGoing) {
-
-            loadCurrentGame();
-
-            addWordsMenu();
-
-            String wordToGuess = wordList.getRandomWordSelected();
-            Hangman gameState = new Hangman(wordToGuess);
-
-            if (wordToGuess == null) {
-                System.out.println("All words have been used. Game over.");
-                break;
-            }
-
-            while (!gameState.isGameOver()) {
-                displayHangmanGame(gameState);
-                char guess = guessLetterMenu();
-                gameState.guessNextLetter(guess);
-            }
-
-            displayEndGame(gameState.isGameOver() && gameState.wordGuessed(), wordToGuess);
-
-            wordList.addWordToList(wordToGuess);
-
-            keepGoing = playNextRound();
-
-            saveCurrentGame();
-        }
-
-        System.out.println("GoodBye!");
+    // EFFECTS: gameplay panel used for guessing letters
+    private void setupGameplayComponents() {
+        // TODO
     }
 
-    // EFFECTS: asks player if they want to continue playing or quit
-    public boolean playNextRound() {
-        System.out.println("\nDo you want to continue playing? (yes/no)");
-        String input = scanner.nextLine().trim().toLowerCase();
-        return input.equals("yes");
-    }
-
-    // EFFECTS: asks player if they want to save the game
-    public void saveCurrentGame() {
-        System.out.println("Do you want to save the game state? (yes/no)");
-        String input = scanner.nextLine().trim().toLowerCase();
-        if ("yes".equals(input)) {
-            saveGame();
-        }
-    }
-
-    // EFFECTS: asks player if they want to save the game
-    public void loadCurrentGame() {
-        System.out.println("Do you want to load the saved game? (yes/no)");
-        String input = scanner.nextLine().trim().toLowerCase();
-        if ("yes".equals(input)) {
-            loadGame();
-        }
+    // EFFECTS: determines if game will be ready to be played or not after adding words to list
+    private void setGameplayEnabled(boolean enabled) {
+        // TODO
     }
 
     // EFFECTS: displays the current game state after word is selected at random
-    public void displayHangmanGame(Hangman hangman) {
-        System.out.println("\nCurrent word: " + hangman.getWordWithGuesses());
-        System.out.println("Attempts left: " + hangman.getLivesLeft());
-        System.out.println("Guessed letters: " + hangman.getGuessedLetters());
+    private void updateGameDisplay() {
+        // TODO
     }
 
-    // EFFECTS: prompts users to guess a letter and displays a list of all letters guessed
-    public char guessLetterMenu() {
-        System.out.print("Guess a letter: ");
-        String line = scanner.nextLine().trim().toLowerCase();
-        while (line.isEmpty() || !Character.isLetter(line.charAt(0))) {
-            System.out.print("Invalid input. Try again: ");
-            line = scanner.nextLine().trim().toLowerCase();
-        }
-        return line.charAt(0);
+    // EFFECTS: sets the panel for the top-portion of the GUI
+    private JPanel setupTopPanel() {
+        // TODO
+        return null;
     }
 
-    // EFFECTS: prompts users to view the list of words added in the list
-    public void displayWordsMenu() {
-        List<String> words = wordList.getWordList();
-
-        if (words.isEmpty()) {
-            System.out.println("No words have been added yet.");
-            return;
-        }
-        System.out.println("Words in the list:");
-        for (String word : words) {
-            System.out.println("- " + word);
-        }
+    // EFFECTS: sets the panel for the center-portion of the GUI
+    private JPanel setupCenterPanel() {
+        // TODO
+        return null;
     }
 
-    // EFFECTS: displays the outcome result of winning the game
-    public void displayEndGame(boolean win, String word) {
-        if (win) {
-            System.out.println("\nCongratulations! You've guessed the word: " + word);
-        } else {
-            System.out.println("\nGame over! The correct word was: " + word);
-        }
+    // EFFECTS: sets the panel for the bottom-portion of the GUI
+    private JPanel setupBottomPanel() {
+        // TODO
+        return null;
     }
 
-    // EFFECTS: saves the current game state to file
-    private void saveGame() {
-        try {
-            jsonWriter.open();
-            jsonWriter.write(hangmanGame, wordList);
-            jsonWriter.close();
-            System.out.println("Saved game to " + JSON_STORE);
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to write to file: " + JSON_STORE);
+    // EFFECTS: sets the game panel
+    private JPanel setupGamePanel() {
+        // TODO
+        return null;
+    }
+
+    // EFFECTS: starts a new game or resets the game state
+    private void startNewGame() {
+        // TODO
+    }
+
+    // EFFECTS: this will determine what will happen when "Start New Game" button is pressed
+    private class StartGameListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // TODO
         }
     }
 
-    // MODIFIES: this
-    // EFFECTS: loads the game state from file
-    private void loadGame() {
-        try {
-            hangmanGame = jsonReader.read();
-            wordList = jsonReader.readWordList();
-            System.out.println("Loaded game from " + JSON_STORE);
-        } catch (IOException e) {
-            System.out.println("Unable to read from file: " + JSON_STORE);
+    // EFFECTS: this will determine what will happen when "guess" button is pressed
+    private class GuessLetterListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // TODO
+        }
+    }
+
+    // EFFECTS: this will determine what will happen when "Add Word" button is pressed
+    private class AddWordListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // TODO
+        }
+    }
+
+    // EFFECTS: this will determine what will happen when "View Words" button is pressed
+    private class ViewWordsListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // TODO
+        }
+    }
+
+    // EFFECTS: this will determine what will happen when "Save Game" button is pressed
+    private class SaveGameListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // TODO
+        }
+    }
+
+    // EFFECTS: this will determine what will happen when "Load Game" button is pressed
+    private class LoadGameListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            // TODO
         }
     }
 }
